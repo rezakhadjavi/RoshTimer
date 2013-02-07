c1d2a77decf44eb14aa179f28270119ca2cf822c
Hamed Al-Khabaz <dota@hotmail.ca>
2013-02-07 09:52:22 -0500

    Roshtimer initialized

---
+++ js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function() {
+	var currentTimeNode = $('.currentTime'),
+		roshTimeNode = $('.roshCountdown'),
+		roshIsDeadNode = $('.btnRoshanDead'),
+		whoKilledNode = $('.whoKilled');
+
+	var isNegativeTime = true,
+		killCounter = 0,
+		isPaused = false;
+
+	var roshHP = 7500,
+		roshDMG = 65,
+		roshARMOR = 3;
+
+	var init = function() {
+		whoKilledNode.hide();
+		currentTimeNode.addClass('negative');
+	};
+
+	init();
+
+	function globalTimeUp() {
+		isNegativeTime = false;
+		currentTimeNode.countdown('destroy');
+		currentTimeNode.removeClass('negative');
+		setTimeout(function() {
+			currentTimeNode.countdown({
+				since: +0,
+				compact: true,
+				format: 'MS',
+				description: ''
+			});
+		}, 50);
+	}
+
+	function globalTimeDown() {
+		currentTimeNode.countdown({
+			until: +90,
+			compact: true,
+			format: 'MS',
+			onExpiry: globalTimeUp,
+			description: ''
+		});
+	}
+
+	function roshRespawn() {
+		whoKilledNode.hide();
+		roshIsDeadNode.show();
+		roshTimeNode.countdown('destroy');
+		roshTimeNode.text("10:00");
+	}
+
+	roshIsDeadNode.click(function(){
+		var row, currentTime;
+		killCounter += 1;
+		$(this).hide();
+		whoKilledNode.show();
+		roshTimeNode.countdown({
+			until: '10m',
+			compact: true,
+			format: 'MS',
+			onExpiry: roshRespawn,
+			description: ''
+		});
+
+		currentTime = isNegativeTime ? "-" + currentTimeNode.text() : currentTimeNode.text();
+		row = "<tr><td>" + killCounter + "</td><td>" + currentTime + "</td><td class='team_" + killCounter + "'></td><td>" + roshHP + "</td></tr>";
+		$('.roshTable tbody').append(row);
+	});
+
+	$('.pause').click(function(){
+		if (isPaused) {
+			isPaused = false;
+			$(this).text('Pause');
+			currentTimeNode.countdown('resume');
+			roshTimeNode.countdown('resume');
+			roshIsDeadNode.removeAttr('disabled');
+		} else {
+			isPaused = true;
+			$(this).text('Resume');
+			currentTimeNode.countdown('pause');
+			roshTimeNode.countdown('pause');
+			roshIsDeadNode.attr('disabled', 'disabled');
+		}
+	});
+
+	$('.whoKilled button').click(function(){
+		var isRadiant = $(this).hasClass('radiant');
+		whoKilledNode.hide();
+
+		if (isRadiant) {
+			$('.team_' + killCounter).text('Radiant');
+		} else {
+			$('.team_' + killCounter).text('Dire');
+		}
+	});
+
+	$('.startGame').click(function(){
+		$(this).parent().hide();
+		$('.roshTimer').show();
+		globalTimeDown();
+	});
+
+	$('.newGame').click(function(){
+		isNegativeTime = true;
+		killCounter = 0;
+		isPaused = false;
+
+		$('.roshTimer').hide();
+		$('.landing').show();
+
+		$('.pause').text("Pause");
+
+		currentTimeNode.countdown('resume');
+		roshTimeNode.countdown('resume');
+		currentTimeNode.countdown('destroy');
+		roshTimeNode.countdown('destroy');
+
+		roshIsDeadNode.removeAttr('disabled');
+		roshIsDeadNode.show();
+		roshTimeNode.text("10:00");
+
+		$('.roshTable tbody tr').remove();
+
+		init();
+	});
+});
