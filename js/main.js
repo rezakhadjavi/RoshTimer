c7013b08a7472c713cbf47e4b90e535880f86207
wololodev <dota@hotmail.ca>
2013-02-07 11:41:16 -0500

    Implemented Rosh Calculations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var currentTimeNode = $('.currentTime'),
 		roshTimeNode = $('.roshCountdown'),
+		roshStatsTimeNode = $('.statsTimer'),
 		roshIsDeadNode = $('.btnRoshanDead'),
 		whoKilledNode = $('.whoKilled');
 
@@ -23,6 +24,7 @@ $(document).ready(function() {
 		isNegativeTime = false;
 		currentTimeNode.countdown('destroy');
 		currentTimeNode.removeClass('negative');
+		instantiateStatsTimer();
 		setTimeout(function() {
 			currentTimeNode.countdown({
 				since: +0,
@@ -35,7 +37,7 @@ $(document).ready(function() {
 
 	function globalTimeDown() {
 		currentTimeNode.countdown({
-			until: +90,
+			until: +90, //1:30
 			compact: true,
 			format: 'MS',
 			onExpiry: globalTimeUp,
@@ -48,6 +50,29 @@ $(document).ready(function() {
 		roshIsDeadNode.show();
 		roshTimeNode.countdown('destroy');
 		roshTimeNode.text("10:00");
+	}
+
+	function instantiateStatsTimer() {
+		roshStatsTimeNode.countdown({
+			since: +0,
+			tickInterval: 300, //5:00
+			onTick: updateRoshStats,
+			compact: true,
+			format: 'MS',
+			description: ''
+		});
+	}
+
+	function updateRoshStats(periods) {
+		var minutes = Number(periods[5]);
+		if (minutes >= 5 && minutes % 5 === 0 && minutes <= 45) {
+			roshHP += 500;
+			roshDMG += 10;
+			roshARMOR += 0.5;
+			$('.roshHP').text(roshHP);
+			$('.roshDMG').text(roshDMG);
+			$('.roshARMOR').text(roshARMOR);
+		}
 	}
 
 	roshIsDeadNode.click(function(){
@@ -74,12 +99,14 @@ $(document).ready(function() {
 			$(this).text('Pause');
 			currentTimeNode.countdown('resume');
 			roshTimeNode.countdown('resume');
+			roshStatsTimeNode.countdown('resume');
 			roshIsDeadNode.removeAttr('disabled');
 		} else {
 			isPaused = true;
 			$(this).text('Resume');
 			currentTimeNode.countdown('pause');
 			roshTimeNode.countdown('pause');
+			roshStatsTimeNode.countdown('pause');
 			roshIsDeadNode.attr('disabled', 'disabled');
 		}
 	});
@@ -105,16 +132,24 @@ $(document).ready(function() {
 		isNegativeTime = true;
 		killCounter = 0;
 		isPaused = false;
+		roshHP = 7500;
+		roshDMG = 65;
+		roshARMOR = 3;
 
 		$('.roshTimer').hide();
 		$('.landing').show();
 
 		$('.pause').text("Pause");
+		$('.roshHP').text(roshHP);
+		$('.roshDMG').text(roshDMG);
+		$('.roshARMOR').text(roshARMOR);
 
 		currentTimeNode.countdown('resume');
 		roshTimeNode.countdown('resume');
+		roshStatsTimeNode.countdown('resume');
 		currentTimeNode.countdown('destroy');
 		roshTimeNode.countdown('destroy');
+		roshStatsTimeNode.countdown('destroy');
 
 		roshIsDeadNode.removeAttr('disabled');
 		roshIsDeadNode.show();
